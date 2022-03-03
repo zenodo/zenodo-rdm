635a0c9b62b39e939a93311daa686f3f957aff2d
Pablo Panero <pablo.panero@cern.ch>
2022-03-03 13:18:22 +0100

    instance: add clean redeploy script

---
+++ wipe_recreate.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Demo-InvenioRDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+# Wipe
+# ----
+
+invenio shell --no-term-title -c "import redis; redis.StrictRedis.from_url(app.config['CACHE_REDIS_URL']).flushall(); print('Cache cleared')"
+# NOTE: db destroy is not needed since DB keeps being created
+#       Just need to drop all tables from it.
+invenio db drop --yes-i-know
+invenio index destroy --force --yes-i-know
+invenio index queue init purge
+
+# Recreate
+# --------
+# NOTE: db init is not needed since DB keeps being created
+#       Just need to create all tables from it.
+invenio db create
+# TODO: add back when pipenv access problem is fixed
+#invenio files location create --default 'default-location'  $(pipenv run invenio shell --no-term-title -c "print(app.instance_path)")'/data'
+invenio files location create --default 'default-location' /opt/invenio/var/instance/data
+invenio roles create admin
+invenio access allow superuser-access role admin
+invenio index init --force
+
+# Add demo and fixtures data
+# -------------
+invenio rdm-records fixtures
+invenio vocabularies import -v names -f ./app_data/vocabularies-future.yaml  # zenodo specific names
+# cannot create records since fixtures are async and might not be present
+# invenio rdm-records demo
+
+# Enable admin user
+invenio users activate admin@inveniosoftware.org
