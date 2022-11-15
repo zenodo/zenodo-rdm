87b9f6e46236c89c17971a5cc6a660154ba8427f
Alex Ioannidis <a.ioannidis@cern.ch>
2022-11-15 18:48:51 +0100

    migration: fix clean-up recipe

--- migration/components/clean_db_tables.py
+++ migration/components/clean_db_tables.py
@@ -1,11 +1,18 @@
-from invenio_rdm_records.records.models import RDMParentMetadata
-from invenio_records.models import RecordMetadata
+from invenio_rdm_records.records.models import (
+    RDMParentMetadata,
+    RDMDraftMetadata,
+    RDMRecordMetadata,
+    RDMVersionsState,
+)
 from invenio_db import db
 from invenio_pidstore.models import PersistentIdentifier
 
-[db.session.delete(item) for item in RecordMetadata.query.all()]
-db.session.commit()
-[db.session.delete(item) for item in RDMParentMetadata.query.all()]
-db.session.commit()
-[db.session.delete(item) for item in PersistentIdentifier.query.all()]
+PersistentIdentifier.query.filter(
+    PersistentIdentifier.pid_type.in_(("recid", "doi", "oai")),
+    PersistentIdentifier.object_type == "rec",
+).delete()
+RDMVersionsState.query.delete()
+RDMRecordMetadata.query.delete()
+RDMParentMetadata.query.delete()
+RDMDraftMetadata.query.delete()
 db.session.commit()
