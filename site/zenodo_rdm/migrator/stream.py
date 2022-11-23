3c05404741531bb309577db3f2ea5f6df9173336
Pablo Panero <pablo.panero@cern.ch>
2022-11-23 17:10:20 +0100

    migrator: use streams runner and yaml config file

--- site/zenodo_rdm/migrator/stream.py
+++ site/zenodo_rdm/migrator/stream.py
@@ -1,17 +1,15 @@
 
-from invenio_rdm_migrator.streams import Stream
+from invenio_rdm_migrator.streams import StreamDefinition
 from invenio_rdm_migrator.streams.records.load import RDMRecordCopyLoad
 
 from extract import JSONLExtract
 from transform import ZenodoToRDMRecordTransform
 
 
-class RecordStream(Stream):
-    """ETL stream for Zenodo to RDM records."""
-
-    def __init__(self, filename, db_uri, output_path):
-        """Constructor."""
-        super().__init__(
-            extract=JSONLExtract(filename),
-            transform=ZenodoToRDMRecordTransform(),
-            load=RDMRecordCopyLoad(db_uri, output_path),
+RecordStreamDefinition = StreamDefinition(
+    name="records",
+    extract_cls=JSONLExtract,
+    transform_cls=ZenodoToRDMRecordTransform,
+    load_cls=RDMRecordCopyLoad,
+)
+"""ETL stream for Zenodo to RDM records."""
