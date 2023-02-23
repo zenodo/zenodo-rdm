58780d8c6b4c6747b1c2f5ecfb2982fe2b2213e4
Pablo Panero <pablo.panero@cern.ch>
2023-02-23 15:39:47 +0100

    oauthclient: enable scopes configuration for keycloak

---
+++ site/zenodo_rdm/oauthclient/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# ZenodoRDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""OAuth client configurations."""
+
+from .openaire_aai import KeycloakSettingsHelper
+
+__all__ = ("KeycloakSettingsHelper",)
