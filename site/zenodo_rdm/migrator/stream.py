1543c776bb570b7c87c827b04a3329356daddc95
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-04-27 18:23:25 +0200

    files: add custom stream for load

--- site/zenodo_rdm/migrator/stream.py
+++ site/zenodo_rdm/migrator/stream.py
@@ -6,9 +6,18 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 """Migrator stream definitions."""
 
-from invenio_rdm_migrator.streams import StreamDefinition
+import contextlib
+
+from datetime import datetime
+from pathlib import Path
+
+from invenio_rdm_migrator.streams import StreamDefinition, Stream
 from invenio_rdm_migrator.streams.communities import CommunityCopyLoad
-from invenio_rdm_migrator.streams.files import FilesCopyLoad
+from invenio_rdm_migrator.streams.files.models import (
+    FilesBucket,
+    FilesInstance,
+    FilesObjectVersion,
+)
 from invenio_rdm_migrator.streams.records import RDMRecordCopyLoad
 from invenio_rdm_migrator.streams.requests import RequestCopyLoad
 from invenio_rdm_migrator.streams.users import UserCopyLoad
@@ -56,10 +65,86 @@ RequestStreamDefinition = StreamDefinition(
 """ETL stream for Zenodo to import users."""
 
 
-FilesStreamDefinition = StreamDefinition(
-    name="files",
-    extract_cls=JSONLExtract,
-    transform_cls=ZenodoFilesTransform,
-    load_cls=FilesCopyLoad,
-)
+# TODO: move this to a proper place or support custom stream classes
+class FileStream(Stream):
+    def __init__(self, db_uri, tmp_dir):
+        """Constructor."""
+        self.tmp_dir = tmp_dir
+        self.db_uri = db_uri
+        self.model_map = {
+            "files_bucket": FilesBucket,
+            "files_files": FilesInstance,
+            "files_object": FilesObjectVersion,
+        }
+
+    def _load(self):
+        """Bulk load CSV table files.
+
+        Loads the tables in the order given by the generator.
+        """
+        import psycopg
+
+        def _ts(iso=True):
+            """Current timestamp string."""
+            dt = datetime.now()
+            return dt.isoformat() if iso else dt.timestamp()
+
+        def get_cols(model):
+            from dataclasses import fields
+
+            return ", ".join([f.name for f in fields(model)])
+
+        with psycopg.connect(self.db_uri) as conn:
+            for name in self.model_map.keys():
+                fpath = Path(self.tmp_dir) / f"{name}.csv"
+                cols = get_cols(self.model_map[name])
+                if fpath.exists():
+                    # total file size for progress logging
+                    file_size = fpath.stat().st_size
+
+                    print(f"[{_ts()}] COPY FROM {fpath}")  # TODO: logging
+                    with contextlib.ExitStack() as stack:
+                        cur = stack.enter_context(conn.cursor())
+                        copy = stack.enter_context(
+                            cur.copy(f"COPY {name} ({cols}) FROM STDIN (FORMAT csv)")
+                        )
+                        fp = stack.enter_context(open(fpath, "r"))
+
+                        block_size = 8192
+
+                        def _data_blocks(block_size=8192):
+                            data = fp.read(block_size)
+                            while data:
+                                yield data
+                                data = fp.read(block_size)
+
+                        for idx, block in enumerate(_data_blocks(block_size)):
+                            if idx % 100:
+                                cur_bytes = idx * block_size
+                                percentage = (cur_bytes / file_size) * 100
+                                progress = (
+                                    f"{cur_bytes}/{file_size} ({percentage:.2f}%)"
+                                )
+                                print(f"[{_ts()}] {name}: {progress}")
+                            copy.write(block)
+                else:
+                    # FIXME: log a WARNING/ERROR
+                    print(f"[{_ts()}] {name}: no data to load")
+                conn.commit()
+
+    def run(self, cleanup=False):
+        """Run ETL stream."""
+        from datetime import datetime
+
+        start_time = datetime.now()
+        print(f"Stream started {start_time.isoformat()}")
+
+        self._load()
+
+        end_time = datetime.now()
+        print(f"Stream ended {end_time.isoformat()}")
+
+        print(f"Execution time: {end_time - start_time}")
+
+
 """ETL stream for Zenodo to import files."""
