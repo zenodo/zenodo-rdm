6f81ad44b8190a2bb71f8a51bf4fb7c7efc4bbbc
anikachurilova <nyuta.churilova@gmail.com>
2023-05-08 17:39:11 +0200

    requests: add a legacy record upgrade request
    * add submit, accept, decline and expire actions logic
    * closes https://github.com/zenodo/zenodo-rdm/issues/284

---
+++ site/zenodo_rdm/legacy/requests/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# Zenodo is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Request types for ZenodoRDM."""
+
+
+from .record_upgrade import LegacyRecordUpgrade
+
+__all__ = ("LegacyRecordUpgrade",)
