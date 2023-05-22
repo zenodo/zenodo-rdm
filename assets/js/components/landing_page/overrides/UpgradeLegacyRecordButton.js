4f53faa21075ddd700edcdbe0ffe14d8cff3b0d8
anikachurilova <nyuta.churilova@gmail.com>
2023-05-22 18:14:05 +0200

    requests: add ui for upgrade legacy record request
    * override record management component to include upgrade legacy record button
    * use community-submission request jinja template for upgrade legacy record request
    * closes https://github.com/zenodo/zenodo-rdm/issues/333

---
+++ assets/js/components/landing_page/overrides/UpgradeLegacyRecordButton.js
@@ -0,0 +1,98 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2023 CERN.
+//
+// Zenodo RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import React, { Component } from "react";
+import { Button, Grid, Message, Icon } from "semantic-ui-react";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+import { NewVersionButton } from "@js/invenio_rdm_records/";
+import PropTypes from "prop-types";
+import { http, withCancel } from "react-invenio-forms";
+
+export class UpgradeLegacyRecordButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      legacyRequestId: "",
+      fetchRequestsError: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const { record, currentUserId } = this.props;
+    if (record.parent.access.owned_by[0].user == currentUserId) {
+      this.fetchRequests();
+    }
+  }
+
+  fetchRequests = async () => {
+    const { record } = this.props;
+    try {
+      const cancellableFetch = withCancel(
+        http.get(
+          `${record.links.requests}?q=type:legacy-record-upgrade&is_open=true`,
+          {
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        )
+      );
+
+      const response = await cancellableFetch.promise;
+      const result = response.data.hits;
+      if (result.total > 0) {
+        this.setState({ legacyRequestId: result.hits[0].id });
+      }
+    } catch (error) {
+      let errorMessage = error.message;
+      if (error.response) {
+        errorMessage = error.response.data.message;
+      }
+
+      console.error(error);
+      this.setState({ fetchRequestsError: errorMessage });
+    }
+  };
+
+  render() {
+    const { isPreviewSubmissionRequest } = this.props;
+    const { legacyRequestId, fetchRequestsError } = this.state;
+    return (
+      <>
+        {legacyRequestId && !isPreviewSubmissionRequest && (
+          <Grid.Column className="pt-0">
+            <Button
+              fluid
+              color="white"
+              size="medium"
+              onClick={() =>
+                (window.location = `/me/requests/${legacyRequestId}`)
+              }
+              icon
+              labelPosition="left"
+            >
+              <Icon name="settings" />
+              {i18next.t("Upgrade legacy record")}
+            </Button>
+          </Grid.Column>
+        )}
+        {fetchRequestsError && (
+          <Grid.Row className="record-management">
+            <Grid.Column>
+              <Message negative>{fetchRequestsError}</Message>
+            </Grid.Column>
+          </Grid.Row>
+        )}
+      </>
+    );
+  }
+}
+
+UpgradeLegacyRecordButton.propTypes = {
+  isPreviewSubmissionRequest: PropTypes.bool.isRequired,
+  record: PropTypes.object.isRequired,
+  currentUserId: PropTypes.string.isRequired,
+};
--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,1 +1,11 @@
-export const overriddenComponents = {}
+// This file is part of InvenioRDM
+// Copyright (C) 2023 CERN.
+//
+// Zenodo RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import { UpgradeLegacyRecordButton } from "../../components/landing_page/overrides/UpgradeLegacyRecordButton";
+
+export const overriddenComponents = {
+  "InvenioAppRdm.RecordLandingPage.RecordManagement.container": UpgradeLegacyRecordButton,
+};
