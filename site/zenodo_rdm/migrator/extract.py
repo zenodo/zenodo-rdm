36237d888798ddd7c24a9bbe5614209c3553e41a
Pablo Panero <pablo.panero@cern.ch>
2023-06-22 09:49:27 +0200

    migrator: add oauth streams

--- site/zenodo_rdm/migrator/extract.py
+++ site/zenodo_rdm/migrator/extract.py
@@ -296,3 +296,26 @@ COPY (
     ) as deposits
 ) TO STDOUT;
 """
+
+EXTRACT_OAUTH_2_SERVER_TOKENS = """
+COPY (
+    SELECT row_to_json(tokens)
+    FROM (
+        SELECT
+            id,
+            client_id,
+            user_id,
+            token_type,
+            convert_from(access_token, "utf-8") as access_token,
+            convert_from(refresh_token, "utf-8") as refresh_token,
+            expires,
+            _scopes,
+            is_personal,
+            is_internal
+        FROM oauth2server_token
+        WHERE
+             user_id=57245
+    ) as tokens
+    LIMIT 100000
+) TO STDOUT;
+"""
