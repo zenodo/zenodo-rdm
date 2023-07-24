8b326e772d44421d3f0ee4c512be61300214e6fc
Alex Ioannidis <a.ioannidis@cern.ch>
2023-07-24 16:59:53 +0200

    tests: switch to RabbitMQ and add Redis

--- site/run-tests.sh
+++ site/run-tests.sh
@@ -53,7 +53,7 @@ if [[ ${keep_services} -eq 0 ]]; then
 fi
 
 python -m check_manifest
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-redis} --env)"
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-rabbitmq} --cache ${CACHE:-redis} --env)"
 # Note: expansion of pytest_args looks like below to not cause an unbound
 # variable error when 1) "nounset" and 2) the array is empty.
 python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
