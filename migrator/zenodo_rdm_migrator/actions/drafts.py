1063a5c9dfd3cb39ba31c348163d81a0ccb83557
Alex Ioannidis <a.ioannidis@cern.ch>
2023-08-01 08:36:02 +0200

    migrator: update based on actions refactor

--- migrator/zenodo_rdm_migrator/actions/drafts.py
+++ migrator/zenodo_rdm_migrator/actions/drafts.py
@@ -10,7 +10,7 @@
 
 from invenio_rdm_migrator.actions import TransformAction
 from invenio_rdm_migrator.load.postgresql.transactions.operations import OperationType
-from invenio_rdm_migrator.streams.actions import RDMDraftCreateAction
+from invenio_rdm_migrator.streams.actions import DraftCreateAction
 from invenio_rdm_migrator.transform import IdentityTransform
 
 from ..transform.records import ZenodoRecordTransform
@@ -20,13 +20,13 @@ class ZenodoDraftCreateAction(TransformAction):
     """Zenodo to RDM draft creation action."""
 
     name = "create-zenodo-draft"
-    load_cls = RDMDraftCreateAction
+    load_cls = DraftCreateAction
 
     @classmethod
-    def matches(self, tx):  # pragma: no cover
+    def matches_action(cls, tx):  # pragma: no cover
         """Checks if the data corresponds with that required by the action."""
         rules = {
-            "pidstore_pid": OperationType.INSERT,  # should we check recid + depid
+            "pidstore_pid": OperationType.INSERT,
             "files_bucket": OperationType.INSERT,
             "records_metadata": OperationType.INSERT,
         }
@@ -35,7 +35,10 @@ class ZenodoDraftCreateAction(TransformAction):
             table_name = operation["source"]["table"]
 
             rule = rules.pop(table_name, None)
-            if not rule or not rule == operation["op"]:
+            # rule can be None, e.g. draft creation has two rows on pidstore_pid
+            # one for recid and one for depid. the second one should be ignored.
+            # FIXME: should we check pid_types
+            if rule and not rule == operation["op"]:
                 return False
 
         return len(rules) == 0
@@ -57,7 +60,7 @@ class ZenodoDraftCreateAction(TransformAction):
                     operation["after"]
                 )
 
-        return self.data_cls(
+        return dict(
             tx_id=self.tx.id,
             pid=pid,
             bucket=bucket,
