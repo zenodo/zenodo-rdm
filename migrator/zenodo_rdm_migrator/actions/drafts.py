5b03ca3b0fbc190a4f231a821327b071fb104ba0
Pablo Panero <pablo.panero@cern.ch>
2023-08-02 11:49:13 +0200

    actions: fix docstring

--- migrator/zenodo_rdm_migrator/actions/drafts.py
+++ migrator/zenodo_rdm_migrator/actions/drafts.py
@@ -5,7 +5,7 @@
 # Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-"""Invenio RDM migration drafts row load module."""
+"""Invenio RDM migration drafts actions module."""
 
 
 from invenio_rdm_migrator.actions import TransformAction
