9c15f99d33a80c3aa032e72bc5158d54639667cd
Pablo Panero <pablo.panero@cern.ch>
2023-08-11 16:49:10 +0200

    migrator: group action tests in classes

--- migrator/tests/actions/drafts/test_drafts_actions.py
+++ migrator/tests/actions/drafts/test_drafts_actions.py
@@ -15,65 +15,83 @@ from invenio_rdm_migrator.streams.actions import load
 from zenodo_rdm_migrator.actions.transform import DraftCreateAction
 
 
-def test_matches_with_valid_data(secret_keys_state):
-    assert (
-        DraftCreateAction.matches_action(
-            Tx(
-                id=1,
-                operations=[
-                    {
-                        "op": OperationType.INSERT,
-                        "source": {"table": "pidstore_pid"},
-                        "after": {},
-                    },
-                    {
-                        "op": OperationType.INSERT,
-                        "source": {"table": "files_bucket"},
-                        "after": {},
-                    },
-                    {
-                        "op": OperationType.INSERT,
-                        "source": {"table": "records_metadata"},
-                        "after": {},
-                    },
-                ],
-            )
-        )
-        is True
-    )
+class TestDraftCreateAction:
+    """Create draft action tests."""
 
-
-def test_matches_with_invalid_data(secret_keys_state):
-    missing_pid = [
-        {"op": OperationType.INSERT, "source": {"table": "files_bucket"}, "after": {}},
-        {
-            "op": OperationType.INSERT,
-            "source": {"table": "records_metadata"},
-            "after": {},
-        },
-    ]
-
-    missing_bucket = [
-        {"op": OperationType.INSERT, "source": {"table": "pidstore_pid"}, "after": {}},
-        {
-            "op": OperationType.INSERT,
-            "source": {"table": "records_metadata"},
-            "after": {},
-        },
-    ]
-    missing_draft = [
-        {"op": OperationType.INSERT, "source": {"table": "pidstore_pid"}, "after": {}},
-        {"op": OperationType.INSERT, "source": {"table": "files_bucket"}, "after": {}},
-    ]
-
-    for invalid_ops in [missing_pid, missing_bucket, missing_draft]:
+    def test_matches_with_valid_data(self):
         assert (
-            DraftCreateAction.matches_action(Tx(id=1, operations=invalid_ops)) is False
+            DraftCreateAction.matches_action(
+                Tx(
+                    id=1,
+                    operations=[
+                        {
+                            "op": OperationType.INSERT,
+                            "source": {"table": "pidstore_pid"},
+                            "after": {},
+                        },
+                        {
+                            "op": OperationType.INSERT,
+                            "source": {"table": "files_bucket"},
+                            "after": {},
+                        },
+                        {
+                            "op": OperationType.INSERT,
+                            "source": {"table": "records_metadata"},
+                            "after": {},
+                        },
+                    ],
+                )
+            )
+            is True
         )
 
+    def test_matches_with_invalid_data(self):
+        missing_pid = [
+            {
+                "op": OperationType.INSERT,
+                "source": {"table": "files_bucket"},
+                "after": {},
+            },
+            {
+                "op": OperationType.INSERT,
+                "source": {"table": "records_metadata"},
+                "after": {},
+            },
+        ]
 
-def test_transform_with_valid_data(secret_keys_state, create_draft_tx):
-    action = DraftCreateAction(
-        Tx(id=create_draft_tx["tx_id"], operations=create_draft_tx["operations"])
-    )
-    assert isinstance(action.transform(), load.DraftCreateAction)
+        missing_bucket = [
+            {
+                "op": OperationType.INSERT,
+                "source": {"table": "pidstore_pid"},
+                "after": {},
+            },
+            {
+                "op": OperationType.INSERT,
+                "source": {"table": "records_metadata"},
+                "after": {},
+            },
+        ]
+        missing_draft = [
+            {
+                "op": OperationType.INSERT,
+                "source": {"table": "pidstore_pid"},
+                "after": {},
+            },
+            {
+                "op": OperationType.INSERT,
+                "source": {"table": "files_bucket"},
+                "after": {},
+            },
+        ]
+
+        for invalid_ops in [missing_pid, missing_bucket, missing_draft]:
+            assert (
+                DraftCreateAction.matches_action(Tx(id=1, operations=invalid_ops))
+                is False
+            )
+
+    def test_transform_with_valid_data(self, create_draft_tx):
+        action = DraftCreateAction(
+            Tx(id=create_draft_tx["tx_id"], operations=create_draft_tx["operations"])
+        )
+        assert isinstance(action.transform(), load.DraftCreateAction)
