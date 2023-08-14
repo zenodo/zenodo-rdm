1f12a7533a03338d083f887885aec3d1186f274f
Pablo Panero <pablo.panero@cern.ch>
2023-08-14 15:25:02 +0200

    migrator: add publish new draft action

--- migrator/tests/actions/drafts/test_drafts_actions.py
+++ migrator/tests/actions/drafts/test_drafts_actions.py
@@ -12,7 +12,11 @@ from invenio_rdm_migrator.extract import Tx
 from invenio_rdm_migrator.load.postgresql.transactions.operations import OperationType
 from invenio_rdm_migrator.streams.actions import load
 
-from zenodo_rdm_migrator.actions.transform import DraftCreateAction, DraftEditAction
+from zenodo_rdm_migrator.actions.transform import (
+    DraftCreateAction,
+    DraftEditAction,
+    DraftPublishAction,
+)
 
 
 class TestDraftCreateAction:
@@ -158,3 +162,366 @@ class TestDraftEditAction:
             Tx(id=update_draft_tx["tx_id"], operations=update_draft_tx["operations"])
         )
         assert isinstance(action.transform(), load.DraftEditAction)
+
+
+class TestDraftPublishAction:
+    """Publish draft action tests."""
+
+    def test_matches_with_valid_data(self):
+        assert (
+            DraftPublishAction.matches_action(
+                Tx(
+                    id=1,
+                    operations=[
+                        {  # draft
+                            "source": {"table": "pidstore_pid"},
+                            "op": OperationType.UPDATE,
+                            "after": {"pid_type": "recid", "id": 1, "status": "K"},
+                        },
+                        {  # draft
+                            "source": {"table": "pidstore_pid"},
+                            "op": OperationType.UPDATE,
+                            "after": {"pid_type": "recid", "id": 1, "status": "R"},
+                        },
+                        {  # parent
+                            "source": {"table": "pidstore_pid"},
+                            "op": OperationType.UPDATE,
+                            "after": {"pid_type": "recid", "id": 2, "status": "R"},
+                        },
+                        {  # draft
+                            "source": {"table": "pidstore_pid"},
+                            "op": OperationType.UPDATE,
+                            "after": {"pid_type": "recid", "id": 1, "status": "R"},
+                        },
+                        {  # parent
+                            "source": {"table": "pidstore_pid"},
+                            "op": OperationType.UPDATE,
+                            "after": {"pid_type": "recid", "id": 2, "status": "M"},
+                        },
+                        {
+                            "source": {"table": "pidstore_redirect"},
+                            "op": OperationType.INSERT,
+                        },
+                        {
+                            "source": {"table": "pidrelations_pidrelation"},
+                            "op": OperationType.DELETE,
+                        },
+                    ],
+                )
+            )
+            is True
+        )
+
+    def test_matches_with_invalid_data(self):
+        missing_pidrel = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+        ]
+
+        missing_pidredirect = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        missing_one_draft = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        missing_one_parent = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        wrong_draft_status = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        wrong_parent_status = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        no_draft = [
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        no_parent = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        wrong_op_type_draft = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.INSERT,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        wrong_op_type_parent = [
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "K"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.INSERT,
+                "after": {"pid_type": "recid", "id": 2, "status": "R"},
+            },
+            {  # draft
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 1, "status": "R"},
+            },
+            {  # parent
+                "source": {"table": "pidstore_pid"},
+                "op": OperationType.UPDATE,
+                "after": {"pid_type": "recid", "id": 2, "status": "M"},
+            },
+            {"source": {"table": "pidstore_redirect"}, "op": OperationType.INSERT},
+            {
+                "source": {"table": "pidrelations_pidrelation"},
+                "op": OperationType.DELETE,
+            },
+        ]
+
+        for invalid_ops in [
+            missing_pidredirect,
+            missing_pidrel,
+            missing_one_draft,
+            missing_one_parent,
+            wrong_parent_status,
+            wrong_draft_status,
+            no_draft,
+            no_parent,
+            wrong_op_type_draft,
+            wrong_op_type_parent,
+        ]:
+            assert (
+                DraftPublishAction.matches_action(Tx(id=1, operations=invalid_ops))
+                is False
+            )
+
+    def test_transform_with_valid_data(self, create_draft_tx):
+        action = DraftPublishAction(
+            Tx(id=create_draft_tx["tx_id"], operations=create_draft_tx["operations"])
+        )
+        assert isinstance(action.transform(), load.DraftPublishAction)
