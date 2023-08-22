f346da68a07c55f233017caa8cdf276fc09430b2
Alex Ioannidis <a.ioannidis@cern.ch>
2023-08-23 01:37:05 +0200

    tests: refactor DB session fixture usage

--- migrator/zenodo_rdm_migrator/actions/transform/users.py
+++ migrator/zenodo_rdm_migrator/actions/transform/users.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2023 CERN.
 #
-# Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
+# ZenodoRDM is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Invenio RDM migration users actions module."""
