b0b09d02238aa0e491a4d8e2c4b13195315a54a9
Jenny Bonsak <jenny.bonsak@gmail.com>
2023-09-15 09:41:05 +0200

    facets: add community facets

---
+++ site/zenodo_rdm/facets.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# ZenodoRDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+from invenio_i18n import gettext as _
+from invenio_records_resources.services.records.facets import (
+    TermsFacet,
+)
+from invenio_vocabularies.contrib.funders.facets import FundersLabels
+from invenio_vocabularies.contrib.affiliations.facets import AffiliationsLabels
+
+funder = TermsFacet(
+    field="metadata.funding.funder.id",
+    label=_("Funders"),
+    value_labels=FundersLabels("funders"),
+)
+
+organization = TermsFacet(
+    field="metadata.organizations.id",
+    label=_("Organizations"),
+    value_labels=AffiliationsLabels("affiliations"),
+)
+
+visibility = TermsFacet(
+    field="access.visibility",
+    label=_("Visibility"),
+    value_labels={
+        "restricted": _("Restricted"),
+    },
+)
