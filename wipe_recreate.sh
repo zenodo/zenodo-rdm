f58f9feba77f7b9790dc922b5d2d17fdfe39d5a9
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-09-19 16:19:41 +0200

    wipe-recreate: create missing roles

--- wipe_recreate.sh
+++ wipe_recreate.sh
@@ -32,10 +32,19 @@ else
   #       Just need to create all tables from it.
   invenio db create
   invenio files location create --default 'default-location'  $(invenio shell --no-term-title -c "print(app.instance_path)")'/data'
+  #
+  # Create roles
+  #
+  # Superuser role
   invenio roles create admin
-  invenio roles create administration-moderation
   invenio access allow superuser-access role admin
+  # Administration access role
+  invenio roles create administration
+  invenio access allow administration-access role administration
+  # Administration moderation role
+  invenio roles create administration-moderation
   invenio access allow administration-moderation role administration-moderation
+
   invenio index init --force
   invenio rdm-records custom-fields init
   invenio communities custom-fields init
