9eb6cdbeb74a5f037545becfbffebcd426c624d3
Pablo Panero <pablo.panero@cern.ch>
2023-09-20 10:04:02 +0200

    migrator: add gh disconnect actions

--- migrator/zenodo_rdm_migrator/actions/transform/oauth.py
+++ migrator/zenodo_rdm_migrator/actions/transform/oauth.py
@@ -295,19 +295,24 @@ class OAuthLinkedAccountDisconnectAction(TransformAction):
     @classmethod
     def matches_action(cls, tx):
         """Checks if the data corresponds with that required by the action."""
-        if len(tx.operations) != 3:
+        if len(tx.operations) > 3 or len(tx.operations) < 2:
             return False
 
         rules = {
             "oauthclient_remoteaccount": OperationType.DELETE,
             "oauthclient_remotetoken": OperationType.DELETE,
-            "oauthclient_useridentity": OperationType.DELETE,
+            "oauthclient_useridentity": OperationType.DELETE,  # optional in gh
         }
 
         for op in tx.operations:
             rule = rules.pop(op["source"]["table"], None)
             if not rule or rule != op["op"]:
                 return False
+
+        # the previous len check means that at this point the rules have 1 or 0 left
+        # if the len is 1 then only left should be the identity
+        if len(rules) == 1 and not "oauthclient_useridentity" in rules.keys():
+            return False
 
         return True
 
@@ -325,9 +330,55 @@ class OAuthLinkedAccountDisconnectAction(TransformAction):
             elif op["source"]["table"] == "oauthclient_useridentity":
                 user_identity = op["before"]
 
-        return {
+        result = {
             "tx_id": self.tx.id,
             "remote_account": IdentityTransform()._transform(remote_account),
             "remote_token": IdentityTransform()._transform(remote_token),
+        }
+
+        if user_identity:
+            result["user_identity"] = IdentityTransform()._transform(user_identity)
+
+        return result
+
+
+class OAuthGHDisconnectToken(TransformAction):
+    """Zenodo to RDM GH linked account disconnect server token and identity."""
+
+    name = "oauth-gh-application-disconnect"
+    load_cls = load.OAuthGHDisconnectToken
+
+    @classmethod
+    def matches_action(cls, tx):
+        """Checks if the data corresponds with that required by the action."""
+        if len(tx.operations) != 2:
+            return False
+
+        rules = {
+            "oauthclient_useridentity": OperationType.DELETE,
+            "oauth2server_token": OperationType.DELETE,
+        }
+
+        for op in tx.operations:
+            rule = rules.pop(op["source"]["table"], None)
+            if not rule or rule != op["op"]:
+                return False
+
+        return True
+
+    def _transform_data(self):
+        """Transforms the data and returns dictionary."""
+        token = None
+        user_identity = None
+
+        for op in self.tx.operations:
+            if op["source"]["table"] == "oauth2server_token":
+                token = op["before"]
+            elif op["source"]["table"] == "oauthclient_useridentity":
+                user_identity = op["before"]
+
+        return {
+            "tx_id": self.tx.id,
+            "token": OAuthServerTokenTransform()._transform(token),
             "user_identity": IdentityTransform()._transform(user_identity),
         }
