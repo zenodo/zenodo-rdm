951426897e5bb7e0710f13f29f820acf135d3d1a
Alex Ioannidis <a.ioannidis@cern.ch>
2023-09-23 01:53:14 +0200

    migrator: reorganize action imports

--- migrator/tests/actions/github/test_github_actions.py
+++ migrator/tests/actions/github/test_github_actions.py
@@ -15,7 +15,7 @@ from invenio_rdm_migrator.extract import Tx
 from invenio_rdm_migrator.load.postgresql.transactions.operations import OperationType
 from invenio_rdm_migrator.streams.actions import load
 
-from zenodo_rdm_migrator.actions.transform import (
+from zenodo_rdm_migrator.actions.transform.github import (
     HookEventCreateAction,
     HookEventUpdateAction,
     HookRepoUpdateAction,
