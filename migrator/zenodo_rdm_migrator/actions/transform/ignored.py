b3454a07d7a2b84a6ba4cb94f4e477053e314b69
Alex Ioannidis <a.ioannidis@cern.ch>
2023-09-23 15:45:16 +0200

    migrator: update ignored actions

--- migrator/zenodo_rdm_migrator/actions/transform/ignored.py
+++ migrator/zenodo_rdm_migrator/actions/transform/ignored.py
@@ -13,26 +13,73 @@ from invenio_rdm_migrator.load.postgresql.transactions.operations import Operati
 from invenio_rdm_migrator.streams.actions import load
 
 
-class FileChecksumAction(TransformAction):
+class IgnoredTransformAction(TransformAction):
+    """Transform ignored actions."""
+
+    load_cls = load.IgnoredAction
+
+    def _transform_data(self):
+        """Return nothing."""
+        return {}
+
+
+class FileChecksumAction(IgnoredTransformAction):
     """Zenodo to RDM for file checksum."""
 
     name = "file-checksum"
-    load_cls = load.IgnoredAction
 
     @classmethod
     def matches_action(cls, tx):
-        """Checks for a single ."""
+        """Checks for a single file instance update."""
         table_ops = [(o["source"]["table"], o["op"]) for o in tx.operations]
         if table_ops == [("files_files", OperationType.UPDATE)]:
             changed_keys = tx.operations[0].get("after", {}).keys()
             return {"last_check", "last_check_at"} < changed_keys
         return False
 
-    def _transform_data(self):
-        """Transforms the data and returns an instance of the mapped_cls."""
-        return {}
+
+class UserSessionAction(IgnoredTransformAction):
+    """Zenodo to RDM for user session."""
+
+    name = "user-session"
+
+    @classmethod
+    def matches_action(cls, tx):
+        """Checks for a single ."""
+        table_ops = [(o["source"]["table"], o["op"]) for o in tx.operations]
+        return all(t == "accounts_user_session_activity" for t, _ in table_ops)
+
+
+class GitHubSyncAction(IgnoredTransformAction):
+    """Zenodo to RDM for GitHub sync."""
+
+    name = "gh-sync"
+
+    @classmethod
+    def matches_action(cls, tx):
+        """Checks for a single ."""
+        table_ops = [(o["source"]["table"], o["op"]) for o in tx.operations]
+        return table_ops == [("oauthclient_remoteaccount", OperationType.UPDATE)]
+
+
+class OAuthReLoginAction(IgnoredTransformAction):
+    """Zenodo to RDM for OAuth re-login."""
+
+    name = "oauth-relogin"
+
+    @classmethod
+    def matches_action(cls, tx):
+        """Checks for a single ."""
+        table_ops = [(o["source"]["table"], o["op"]) for o in tx.operations]
+        return table_ops == [
+            ("accounts_user", OperationType.UPDATE),
+            ("oauthclient_remotetoken", OperationType.UPDATE),
+        ]
 
 
 IGNORED_ACTIONS = [
     FileChecksumAction,
+    UserSessionAction,
+    GitHubSyncAction,
+    OAuthReLoginAction,
 ]
