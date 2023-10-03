56c65070a036a9edeebc0968c7c0eeafaa37ad62
Nicola Tarocco <ntarocco@gmail.com>
2023-10-03 11:43:39 +0200

    migration: update migration script

--- migrator/migrate.sh
+++ migrator/migrate.sh
@@ -71,25 +71,102 @@ psql $DB_URI -f scripts/create_missing_indices.sql
 # Fixtures
 invenio rdm-records fixtures
 
-# Reindex records and communities
-invenio rdm-records rebuild-index
+###########################################################
+# Reindex records
+# Vocabularies should be already in the cluster
+<<com
+###########################################################
+# Taken from:
+# invenio rdm-records rebuild-index
+
+from invenio_access.permissions import system_identity
+from invenio_records_resources.proxies import current_service_registry
+from invenio_rdm_recods.proxies import current_rdm_records_service
+
+# Commented out but left these here in case of urgent need
+#vocab_service = current_service_registry.get("vocabularies")
+#vocab_service.rebuild_index(identity=system_identity)
+#names_service = current_service_registry.get("names")
+#names_service.rebuild_index(identity=system_identity)
+#funders_service = current_service_registry.get("funders")
+#funders_service.rebuild_index(identity=system_identity)
+#awards_service = current_service_registry.get("awards")
+#awards_service.rebuild_index(identity=system_identity)
+#subj_service = current_service_registry.get("subjects")
+#subj_service.rebuild_index(identity=system_identity)
+#affs_service = current_service_registry.get("affiliations")
+#affs_service.rebuild_index(identity=system_identity)
+
+# Re-index record/drafts
+current_rdm_records_service.rebuild_index(identity=system_identity)
+com
+
+###########################################################
+# Reindex communities
 invenio communities rebuild-index
 
-# Reindex users
+###########################################################
+# Reindex users, groups, members, archived invitations, requests, events
 # Open an invenio shell and run the command:
 <<com
 from invenio_access.permissions import system_identity
-from invenio_users_resources.proxies import current_users_service
+from invenio_communities.proxies import current_communities
+from invenio_requests.proxies import current_events_service, current_requests_service
+from invenio_rdm_recods.proxies import current_rdm_records_service
+from invenio_users_resources.proxies import current_users_service, current_groups_service
 
-current_users_service.rebuild_index(identity=system_identity)
+# reindex users
+current_users_service.rebuild_index(system_identity)
+
+# reindex groups
+current_groups_service.rebuild_index(system_identity)
+
+# reindex members and archived invitations
+current_communities.service.members.rebuild_index(system_identity)
+
+# reindex requests
+for req_meta in current_requests_service.record_cls.model_cls.query.all():
+    request = current_requests_service.record_cls(req_meta.data, model=req_meta)
+    if not request.is_deleted:
+        current_requests_service.indexer.index(request)
+
+# reindex requests events
+for event_meta in current_events_service.record_cls.model_cls.query.all():
+    event = current_events_service.record_cls(event_meta.data, model=event_meta)
+    current_events_service.indexer.index(event)
 com
 
-# When finished indexing, refresh the index
+# For anything above 10K, manually spawn `process_bulk_queue` to put the Celery workers to work
 <<com
-from invenio_users_resources.proxies import current_users_service
+from invenio_communities.proxies import current_communities
+from invenio_requests.proxies import current_events_service, current_requests_service
+from invenio_rdm_recods.proxies import current_rdm_records_service
+from invenio_users_resources.proxies import current_users_service, current_groups_service
+
+# Records ~3.5M / 10k messages = 350
+for _ in range(350):
+    current_rdm_records_service.indexer.process_bulk_queue()
+# Drafts ~500k / 10k messages = 50
+for _ in range(50):
+    current_rdm_records_service.draft_indexer.process_bulk_queue()
+# Users: ~600k / 10k messages = 60
+for _ in range(60):
+    current_users_service.indexer.process_bulk_queue()
+com
+
+# When finished indexing, refresh the indices
+<<com
+from invenio_users_resources.proxies import current_users_service, current_groups_service
+from invenio_communities.proxies import current_communities
+from invenio_requests.proxies import current_events_service, current_requests_service
 
 current_users_service.indexer.refresh()
+current_groups_service.indexer.refresh()
+current_communities.service.members.indexer.refresh()
+current_requests_service.indexer.refresh()
+current_events_service.indexer.refresh()
+current_rdm_records_service.indexer.refresh()
 com
 
-# Add admin users (Pablo, Alex, Zach, Manuel)
+# Add admin users (Alex, Zach, Manuel)
 sh scripts/add_admins.sh
--- migrator/scripts/add_admins.sh
+++ migrator/scripts/add_admins.sh
@@ -7,7 +7,6 @@ invenio roles create admin
 # invenio access allow superuser-access role admin
 invenio access allow administration-access role admin
 # and then add the role to your user
-invenio roles add ppanero@cern.ch admin  # pablo
 invenio roles add zacharias.zacharodimos@cern.ch admin  # zach
 invenio roles add manuel.alejandro.de.oliveira.da.costa@cern.ch admin  # manuel
 invenio roles add a.ioannidis@cern.ch admin  # alex
