4faac6290b59fa3012f22f592466020440d07c62
Alex Ioannidis <a.ioannidis@cern.ch>
2023-10-08 17:09:17 +0200

    config: fix thumbnails sizes format

--- site/zenodo_rdm/config.py
+++ site/zenodo_rdm/config.py
@@ -330,11 +330,11 @@ RDM_LOCK_EDIT_PUBLISHED_FILES = lock_edit_record_published_files
 """Lock editing already published files (enforce record versioning)."""
 
 APP_RDM_RECORD_THUMBNAIL_SIZES = [
-    "10",
-    "50",
-    "100",
-    "250",
-    "750",
-    "1200",
+    10,
+    50,
+    100,
+    250,
+    750,
+    1200,
 ]
 """Thumbnail sizes."""
