8364721a522bccdc6b4418e31b1cae82374ee7a1
Fatimah <fatimah.zulfiqar@cern.ch>
2024-02-09 11:16:11 +0100

    horizon: update header, footer and redirection
    formatting: fixed formatting issues

--- site/zenodo_rdm/config.py
+++ site/zenodo_rdm/config.py
@@ -10,6 +10,7 @@
 from .params import ZenodoArgsSchema, ZenodoSearchOptions
 from .redirector import (
     communities_detail_view_function,
+    communities_home_view_function,
     communities_records_search,
     communities_requests_view_function,
     communities_settings_view_function,
@@ -231,6 +232,11 @@ REDIRECTOR_RULES = {
         "source": "/communities/<community_id>/edit",
         "rule_options": {"strict_slashes": False},
         "target": communities_settings_view_function,
+    },
+    "redirect_communities_home": {
+        "source": "/communities/<community_id>",
+        "rule_options": {"strict_slashes": False},
+        "target": communities_home_view_function,
     },
     "redirect_communities_search": {
         "source": "/communities/<community_id>/search",
