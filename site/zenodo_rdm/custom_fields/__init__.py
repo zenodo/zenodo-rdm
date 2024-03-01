59aa8a874972a9bea3293f79f9cc0da1e57bda39
Fatimah <fatimah.zulfiqar@cern.ch>
2024-03-01 10:33:26 +0100

    custom_field: added subject field to community

--- site/zenodo_rdm/custom_fields/__init__.py
+++ site/zenodo_rdm/custom_fields/__init__.py
@@ -34,11 +34,6 @@ from invenio_rdm_records.contrib.thesis import (
     THESIS_NAMESPACE,
 )
 
-from .community_fields import (
-    COMMUNITY_FIELDS,
-    COMMUNITY_FIELDS_UI,
-    COMMUNITY_NAMESPACES,
-)
 from .domain_fields import (
     AUDIOVIS_FIELDS,
     AUDIOVIS_FIELDS_UI,
