0e39ec4a8a9b0ba26671f925c9279a9e3c035f13
Alex Ioannidis <a.ioannidis@cern.ch>
2024-03-19 23:46:31 +0100

    scripts: allow filtering deployments

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,37 +4,50 @@ import subprocess
 from pathlib import Path
 
 
-PRGORAM = Path(sys.argv[0]).name
+PROGRAM = Path(sys.argv[0]).name
 ENVS = {
     "dev": "zenodo-rdm-dev",
     "qa": "zenodo-rdm-qa",
     "prod": "zenodo-rdm",
 }
+env_names = ", ".join(ENVS.keys())
 DEPLOYMENTS = {
     "web": ["web", "copy-web-assets"],
     "worker": ["worker"],
     "worker-beat": ["worker-beat"],
+    "terminal": ["terminal"],
 }
-
+deployment_names = ", ".join(DEPLOYMENTS.keys())
 
 def _abort(message):
     print(message)
     sys.exit(1)
 
 
-if len(sys.argv) != 3:
-    print(f"Usage: {PRGORAM} <env> <image_tag>\n")
-    print(f"  env: one of [{', '.join(ENVS.keys())}]")
+if len(sys.argv) < 3:
+    print(f"Usage: {PROGRAM} <env> <image_tag> [<deployment>...]\n")
+    print(f"  env: one of [{env_names}]")
     print("  image_tag: the tag of the image to deploy (e.g. `3.1.0`)")
+    print(f"  deployment: any of [{deployment_names}] (optional, default all)")
     print()
-    print(f"  Example: {PRGORAM} qa 3.1.0")
+    print(f"  Example: {PROGRAM} qa 3.1.0")
     sys.exit(1)
 
 env = sys.argv[1]
-image_tag = sys.argv[2]
-
 if env not in ENVS:
-    _abort(f"Invalid environment: {env}, must be one of {list(ENVS.keys())}")
+    _abort(f"Invalid environment: {env}, must be one of {env_names}")
+
+image_tag = sys.argv[2]
+if not image_tag:
+    _abort("Invalid image tag")
+
+if len(sys.argv) > 3:
+    deployments = sys.argv[3:]
+    for dep in deployments:
+        if dep not in DEPLOYMENTS:
+            _abort(f"Invalid deployment: {dep}, must be one of {deployment_names}")
+else:
+    deployments = DEPLOYMENTS.keys()
 
 # Verify image exists
 image = f"ghcr.io/zenodo/zenodo-rdm/zenodo-rdm:{image_tag}"
@@ -47,13 +60,18 @@ if res.returncode != 0:
 
 oc_env = ENVS[env]
 print(f"You're deploying to {env} ({oc_env}), the following images updates:")
-for deployment, containers in DEPLOYMENTS.items():
-    print(f"  {deployment}: {containers} -> {image}")
+for dep, containers in DEPLOYMENTS.items():
+    if dep not in deployments:
+        continue
+    print(f"  {dep}: {containers} -> {image}")
+
 if input("\nType the name of the environment to confirm: ") != env:
     _abort("Deployment aborted!")
 
 
-for deployment, containers in DEPLOYMENTS.items():
+for dep, containers in DEPLOYMENTS.items():
+    if dep not in deployments:
+        continue
     containers_args = [f"{container}={image}" for container in containers]
     print()
     res = subprocess.run(
@@ -64,7 +82,7 @@ for deployment, containers in DEPLOYMENTS.items():
             "image",
             "--namespace",
             oc_env,
-            f"deployment/{deployment}",
+            f"deployment/{dep}",
             *containers_args,
         ],
         capture_output=True,
