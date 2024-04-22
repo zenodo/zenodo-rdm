195a3c8aee1e8eef465000506c07bee761b5f6a4
Fatimah <fatimah.zulfiqar@cern.ch>
2024-04-22 11:52:49 +0200

    mirador: added check for published record

--- site/zenodo_rdm/previewer/mirador.py
+++ site/zenodo_rdm/previewer/mirador.py
@@ -19,15 +19,15 @@ def can_preview(file):
     # supported_extensions list needs . prefixed -
     preview_extensions = current_app.config["MIRADOR_PREVIEW_EXTENSIONS"]
     supported_extensions = ["." + ext for ext in preview_extensions]
-    return file.has_extensions(*supported_extensions)
+    return (
+        file.has_extensions(*supported_extensions) and file.record.data["is_published"]
+    )
 
 
 def preview(file):
     """Render template."""
-
     return render_template(
         "invenio_app_rdm/records/mirador_preview.html",
         file=file,
-        manifest_url=file.record.links["self_iiif_manifest"],
         ui_config=current_app.config["MIRADOR_PREVIEW_CONFIG"],
     )
