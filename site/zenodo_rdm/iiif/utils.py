a434520cce3baea5e87b443da0caf808f2bbd0db
yashlamba <yashlamba2000@gmail.com>
2024-04-22 16:08:31 +0200

    iiif: fix imports & register iiif tasks

--- site/zenodo_rdm/iiif/utils.py
+++ site/zenodo_rdm/iiif/utils.py
@@ -1,14 +1,15 @@
+"""IIIF Image conversion utils."""
+
 from pathlib import Path
 from textwrap import wrap
 
-from flask import current_app
-
-
-from invenio_rdm_records.records.api import RDMRecord
 import pyvips
+from flask import current_app
+from invenio_rdm_records.records.api import RDMRecord
 
 
 def source_custom(input_file):
+    """Custom source generator for pyvips."""
 
     def read_handler(size):
         return input_file.read(size)
@@ -26,6 +27,8 @@ def source_custom(input_file):
 
 
 def target_custom(output_file):
+    """Custom target generator for pyvips."""
+
     def write_handler(chunk):
         return output_file.write(chunk)
 
@@ -54,14 +57,18 @@ def target_custom(output_file):
 
 
 class ImageConverter:
+    """Base class for Image converters."""
 
     def convert(self, in_stream, out_stream) -> bool:
+        """Convert image."""
         pass
 
 
 class PyVIPSImageConverter(ImageConverter):
+    """Pyvips image converter for pyramidal tifs."""
 
     def __init__(self, params=None):
+        """Constructor."""
         self.params = params or {
             "compression": "jpeg",
             "Q": 90,
@@ -70,6 +77,7 @@ class PyVIPSImageConverter(ImageConverter):
         }
 
     def convert(self, in_stream, out_stream):
+        """Convert to ptifs."""
         try:
             source = source_custom(in_stream)
 
@@ -83,37 +91,52 @@ class PyVIPSImageConverter(ImageConverter):
 
 
 class TilesStorage:
+    """Base class for tile storage."""
 
     def __init__(self, *, converter: ImageConverter):
+        """Constructor."""
         self.converter = converter
 
     def save(self, record: RDMRecord, filename: str):
+        """Save tiles."""
         pass
 
     def open(self, record: RDMRecord, filename: str):
+        """Open file in read mode."""
         pass
 
     def delete(self, record: RDMRecord, filename: str):
+        """Delete tiles file."""
         pass
 
 
 class LocalTilesStorage(TilesStorage):
+    """Local tile storage implementation."""
 
     def __init__(self, *, base_path: str, converter: ImageConverter = None, **kwargs):
+        """Constructor."""
         converter = converter or PyVIPSImageConverter()
         self.base_path = Path(base_path)
         super().__init__(converter=converter, **kwargs)
 
     def _get_dir(self, record: RDMRecord) -> Path:
+        """Get directory."""
         rec_id = record.pid.pid_value
         path_partitions = wrap(rec_id, width=3)
-        return self.base_path / record.access.protection.files / Path(*path_partitions) / rec_id
+        return (
+            self.base_path
+            / record.access.protection.files
+            / Path(*path_partitions)
+            / rec_id
+        )
 
     def _get_file_path(self, record: RDMRecord, filename: str) -> Path:
+        """Get file path."""
         # Partition record.id into 3 chunks of min. 2 characters (e.g. "12345678" -> ["12", "34", "5678"])
         return self._get_dir(record) / (filename + ".ptif")
 
     def save(self, record, filename):
+        """Convert and save to ptif."""
         outpath = self._get_file_path(record, filename)
         if outpath.exists():
             return
@@ -125,8 +148,9 @@ class LocalTilesStorage(TilesStorage):
                 current_app.logger.info(f"Image conversion failed {record.id}")
 
     def open(self, record, filename):
+        """Open the file in read mode."""
         return self._get_file_path(record, filename).open("rb")
 
     def delete(self, record, filename):
-        self._get_file_path(record, filename).unlink()
-        return True
+        """Delete the ptif."""
+        pass
