aead000855a8a1123f93059f9ca26d2cc26d2a23
Alex Ioannidis <a.ioannidis@cern.ch>
2024-07-31 16:43:04 +0200

    sitemap: generate from search and filter unverified

--- site/zenodo_rdm/sitemap/generators.py
+++ site/zenodo_rdm/sitemap/generators.py
@@ -9,76 +9,82 @@
 
 import arrow
 from flask import current_app, url_for
-from invenio_communities.communities.records.models import CommunityMetadata
+from invenio_communities.proxies import current_communities
 from invenio_db import db
-from invenio_pidstore.models import PersistentIdentifier, PIDStatus
-from invenio_rdm_records.records.models import RDMRecordMetadata
-from invenio_records.models import RecordMetadata
+from invenio_rdm_records.proxies import current_rdm_records_service as records_service
+from invenio_search.api import RecordsSearchV2
 
 
-def _sitemapdtformat(dt):
+def _sitemapdtformat(dt_str):
     """Convert a datetime to a W3 Date and Time format.
 
     Converts the date to a minute-resolution datetime timestamp with a special
     UTC designator 'Z'. See more information at
     https://www.w3.org/TR/NOTE-datetime.
     """
-    adt = arrow.Arrow.fromdatetime(dt).to("utc")
-    return adt.format("YYYY-MM-DDTHH:mm:ss") + "Z"
+    return arrow.get(dt_str).format("YYYY-MM-DDTHH:mm:ss") + "Z"
 
 
 def records_generator():
     """Generate the records links."""
-    q = (
-        db.session.query(PersistentIdentifier.pid_value, RDMRecordMetadata.updated)
-        .join(
-            RDMRecordMetadata, RDMRecordMetadata.id == PersistentIdentifier.object_uuid
-        )
-        .filter(
-            PersistentIdentifier.status == PIDStatus.REGISTERED,
-            PersistentIdentifier.pid_type == "recid",
-            RDMRecordMetadata.deletion_status == "P",
-        )
+    search = (
+        RecordsSearchV2(index=records_service.record_cls.index._name)
+        .filter("term", **{"parent.is_verified": True})
+        .filter("term", deletion_status="P")
+        .sort({"updated": "desc"})
+        # This is supposedly bad for performance, and we should isntead use
+        # the `search_after` and PIT search: https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
+        .params(preserve_order=True)
+        .source(["id", "updated"])
     )
 
     scheme = current_app.config["SITEMAP_URL_SCHEME"]
-    for pid_value, updated_at in q.yield_per(1000):
+    for record in search.scan():
         yield {
             "loc": url_for(
                 "invenio_app_rdm_records.record_detail",
-                pid_value=pid_value,
+                pid_value=record.id,
                 _external=True,
                 _scheme=scheme,
             ),
-            "lastmod": _sitemapdtformat(updated_at),
+            "lastmod": _sitemapdtformat(record.updated),
         }
 
 
 def communities_generator():
     """Generate the communities links."""
-    q = db.session.query(CommunityMetadata.slug, CommunityMetadata.updated).filter(
-        CommunityMetadata.deletion_status == "P"
+    search = (
+        RecordsSearchV2(index=current_communities.service.record_cls.index._name)
+        .filter("term", is_verified=True)
+        .filter("term", deletion_status="P")
+        .sort({"updated": "desc"})
+        # This is supposedly bad for performance, and we should isntead use
+        # the `search_after` and PIT search: https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
+        .params(preserve_order=True)
+        .source(["slug", "updated", "metadata.page"])
     )
+
     scheme = current_app.config["SITEMAP_URL_SCHEME"]
-    for comm_slug, updated_at in q.yield_per(1000):
+    for community in search.scan():
         yield {
             "loc": url_for(
                 "invenio_app_rdm_communities.communities_detail",
-                pid_value=comm_slug,
+                pid_value=community.slug,
                 _external=True,
                 _scheme=scheme,
             ),
-            "lastmod": _sitemapdtformat(updated_at),
+            "lastmod": _sitemapdtformat(community.updated),
         }
-        yield {
-            "loc": url_for(
-                "invenio_communities.communities_about",
-                pid_value=comm_slug,
-                _external=True,
-                _scheme=scheme,
-            ),
-            "lastmod": _sitemapdtformat(updated_at),
-        }
+        if "metadata" in community:
+            yield {
+                "loc": url_for(
+                    "invenio_communities.communities_about",
+                    pid_value=community.slug,
+                    _external=True,
+                    _scheme=scheme,
+                ),
+                "lastmod": _sitemapdtformat(community.updated),
+            }
 
 
 generator_fns = [
