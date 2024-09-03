cc03281abe37a3eedd4f8ac5111a4d9239cb36de
Alex Ioannidis <a.ioannidis@cern.ch>
2024-09-03 17:24:22 +0200

    index_templates: configure number of shards for record indices

---
+++ site/zenodo_rdm/index_templates/os-v2/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 CERN.
+#
+# ZenodoRDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Zenodo RDM index templates."""
