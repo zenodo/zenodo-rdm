5e6743972a14cb9448bc917b097060158bafb88d
Fatimah Zulfiqar <fatimah0zulfiqar@gmail.com>
2024-10-16 15:43:32 +0200

    horizon: added collection areas to frontpage

---
+++ site/zenodo_rdm/assets/semantic-ui/js/zenodo_rdm/src/horizon/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { CommunityItem } from "./community-item";
+import { CommunitySelectionSearch } from "@js/invenio_rdm_records";
+
+const defaultProps = {
+  isInitialSubmission: true,
+  apiConfigs: {
+    allCommunities: {
+      initialQueryState: { size: 5, page: 1, sortBy: "newest" },
+      searchApi: {
+        axios: {
+          url: "/api/communities",
+          headers: { Accept: "application/vnd.inveniordm.v1+json" },
+        },
+      },
+      appId: "ReactInvenioDeposit.CommunitySelectionSearch.recentCommunities",
+      toggleText: "Search in all communities",
+    },
+    myCommunities: {
+      initialQueryState: { size: 5, page: 1, sortBy: "bestmatch" },
+      searchApi: {
+        axios: {
+          url: "/api/user/communities",
+          headers: { Accept: "application/vnd.inveniordm.v1+json" },
+        },
+      },
+      appId: "ReactInvenioDeposit.CommunitySelectionSearch.MyCommunities",
+      toggleText: "Search in my communities",
+    },
+  },
+};
+
+ReactDOM.render(
+  <CommunitySelectionSearch
+    apiConfigs={defaultProps.apiConfigs}
+    isInitialSubmission={defaultProps.isInitialSubmission}
+    CommunityListItem={CommunityItem}
+    pagination={false}
+  />,
+  document.getElementById("project-search-menu")
+);
