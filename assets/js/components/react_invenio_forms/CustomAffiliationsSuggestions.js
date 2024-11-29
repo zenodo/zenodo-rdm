57e3ef4fdbe9f661581f87241b2bf1f47fb5f762
Alex Ioannidis <a.ioannidis@cern.ch>
2024-11-29 17:10:06 +0100

    assets: fix affiliations overriddable
    * Fixes the affiliation suggestions to process a single item instead of
      a list of creators/affiliations.

--- assets/js/components/react_invenio_forms/CustomAffiliationsSuggestions.js
+++ assets/js/components/react_invenio_forms/CustomAffiliationsSuggestions.js
@@ -1,59 +1,53 @@
 import React from "react";
 import { Header, Grid } from "semantic-ui-react";
-import { makeIdEntry, makeSubheader } from "react-invenio-forms";
 
+export const CustomAffiliationsSuggestions = ({
+  creatibutor, makeSubheader, makeIdEntry
+}) => {
+  const { name, acronym, identifiers = [], id } = creatibutor;
 
-export const CustomAffiliationsSuggestions = (creatibutors) => {
-  // Ensure creatibutors is always an array
-  const creatibutorsArray = Array.isArray(creatibutors) ? creatibutors : [creatibutors];
+  const subheader = makeSubheader(creatibutor, creatibutor.isOrganization);
+  const displayName = acronym ? `${name} (${acronym})` : name;
 
-  return creatibutorsArray.map((creatibutor) => {
-    const { creatibutor: creatibutorData } = creatibutor;
-    const { name, acronym, identifiers = [], id } = creatibutorData;
+  const sources = [];
+  const idStrings = [];
 
-    const subheader = makeSubheader(creatibutorData, creatibutor.isOrganization);
-    const displayName = acronym ? `${name} (${acronym})` : name;
+  identifiers.forEach((identifier) => {
+    const { scheme, identifier: value } = identifier;
 
-    const sources = [];
-    const idStrings = [];
-
-    identifiers.forEach((identifier) => {
-      const { scheme, identifier: value } = identifier;
-
-      if (scheme === "ror") {
-        sources.push(
-          <span key={value}>
-            Source: {scheme.toUpperCase()} <span>(Preferred)</span>
-          </span>
-        );
-      } else if (scheme === "edmo") {
-        sources.push(
-          <span key={value}>Source: {scheme.toUpperCase()}</span>
-        );
-      } else {
-        const idEntry = makeIdEntry(identifier);
-        if (idEntry) idStrings.push(idEntry);
-      }
-    });
-
-    return (
-      <Grid key={id}>
-        <Grid.Row columns={2} verticalAlign="middle">
-          <Grid.Column width={12}>
-            <Header>
-              {displayName} {idStrings.length > 0 && <>({idStrings})</>}
-              {subheader && <Header.Subheader>{subheader}</Header.Subheader>}
-            </Header>
-          </Grid.Column>
-          <Grid.Column width={4} textAlign="right">
-            {sources.length > 0 && (
-              <Header as="h6" color="grey" style={{ margin: 0 }}>
-                {sources}
-              </Header>
-            )}
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
+    if (scheme === "ror") {
+      sources.push(
+        <span key={value}>
+          Source: {scheme.toUpperCase()} <span>(Preferred)</span>
+        </span>
+      );
+    } else if (scheme === "edmo") {
+      sources.push(
+        <span key={value}>Source: {scheme.toUpperCase()}</span>
+      );
+    } else {
+      const idEntry = makeIdEntry(identifier);
+      if (idEntry) idStrings.push(idEntry);
+    }
   });
+
+  return (
+    <Grid key={id}>
+      <Grid.Row columns={2} verticalAlign="middle">
+        <Grid.Column width={12}>
+          <Header>
+            {displayName} {idStrings.length > 0 && <>({idStrings})</>}
+            {subheader && <Header.Subheader>{subheader}</Header.Subheader>}
+          </Header>
+        </Grid.Column>
+        <Grid.Column width={4} textAlign="right">
+          {sources.length > 0 && (
+            <Header as="h6" color="grey" style={{ margin: 0 }}>
+              {sources}
+            </Header>
+          )}
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 };
