f0f4048fb1e62bc51ffd4ba3b6b16c59433fffa1
yashlamba <yashlamba2000@gmail.com>
2025-01-06 10:46:26 +0100

    curation: process new records created 30 days ago

--- site/zenodo_rdm/curation/tasks.py
+++ site/zenodo_rdm/curation/tasks.py
@@ -32,7 +32,7 @@ Record IDs Approved:
 
 def _send_result_email(content):
     """Send curation result as email."""
-    subject = f"EU Record Curation Processesed {datetime.now().date()}"
+    subject = f"EU Record Curation Processed {datetime.now().date()}"
     body = RESULT_EMAIL_BODY.format(finished_at=datetime.now(timezone.utc), **content)
     sender = current_app.config["MAIL_DEFAULT_SENDER"]
     admin_email = current_app.config["APP_RDM_ADMIN_EMAIL_RECIPIENT"]
@@ -42,6 +42,64 @@ def _send_result_email(content):
     mail_ext = current_app.extensions["mail"]
     msg = Message(subject, sender=sender, recipients=recipients, body=body)
     mail_ext.send(msg)
+
+
+def _get_eu_records_query(since):
+    """Get dsl query for records to be processed."""
+    created_before = datetime.now(timezone.utc) - timedelta(days=30)
+    updated_after = datetime.fromisoformat(since) - timedelta(hours=12)
+
+    # Get records with EC funding and not in EU community already and not created in last 30 days
+    ec_funded = dsl.Q(
+        "bool",
+        must=[
+            dsl.Q("term", **{"metadata.funding.funder.id": "00k4n6c32"}),
+            dsl.Q("term", **{"is_deleted": False}),
+            dsl.Q(
+                "range",
+                created={
+                    "lte": created_before.isoformat(),
+                },
+            ),
+        ],
+        must_not=[
+            dsl.Q(
+                "term",
+                **{
+                    "parent.communities.ids": current_app.config.get(
+                        "EU_COMMUNITY_UUID"
+                    )
+                },
+            ),
+        ],
+    )
+
+    updated_after_since = dsl.Q(
+        "bool",
+        must=[
+            dsl.Q(
+                "range",
+                updated={
+                    "gte": updated_after.isoformat(),
+                },
+            ),
+        ],
+    )
+
+    # Created 31 days before (with a 6 hour buffer)
+    new_created = dsl.Q(
+        "bool",
+        must=[
+            dsl.Q(
+                "range",
+                created={
+                    "gte": (created_before - timedelta(days=1, hours=6)).isoformat(),
+                },
+            ),
+        ],
+    )
+
+    return ec_funded & (updated_after_since | new_created)
 
 
 @shared_task
@@ -56,43 +114,12 @@ def run_eu_record_curation(since):
     }
     dry_run = not current_app.config.get("CURATION_ENABLE_EU_CURATOR")
     curator = EURecordCurator(dry=dry_run)
-    created_before = (datetime.now(timezone.utc) - timedelta(days=30)).isoformat()
-    updated_after = (datetime.fromisoformat(since) - timedelta(hours=12)).isoformat()
 
-    query = dsl.Q(
-        "bool",
-        must=[
-            dsl.Q("term", **{"metadata.funding.funder.id": "00k4n6c32"}),
-            dsl.Q("term", **{"is_deleted": False}),
-            dsl.Q(
-                "range",
-                created={
-                    "lte": created_before,
-                },
-            ),
-            dsl.Q(
-                "range",
-                updated={
-                    "gte": updated_after,
-                },
-            ),
-        ],
-        must_not=[
-            dsl.Q(
-                "term",
-                **{
-                    "parent.communities.ids": current_app.config.get(
-                        "EU_COMMUNITY_UUID"
-                    )
-                },
-            )
-        ],
-    )
     search = records_service.create_search(
         system_identity,
         records_service.record_cls,
         records_service.config.search,
-        extra_filter=query,
+        extra_filter=_get_eu_records_query(since),
     )
 
     for item in search.scan():
