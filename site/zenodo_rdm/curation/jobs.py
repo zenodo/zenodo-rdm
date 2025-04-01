c2fd8d1733c1bf793571de347757883d504caa91
yashlamba <yashlamba2000@gmail.com>
2025-04-01 10:36:59 +0200

    curation: fix job args

--- site/zenodo_rdm/curation/jobs.py
+++ site/zenodo_rdm/curation/jobs.py
@@ -16,28 +16,16 @@ from marshmallow_utils.fields import ISODateString
 from zenodo_rdm.curation.tasks import run_eu_record_curation
 
 
-class ArgsSchema(Schema):
-    """Schema of task input arguments."""
-
-    since = ISODateString()
-    job_arg_schema = fields.String(
-        metadata={"type": "hidden"},
-        dump_default="ArgsSchema",
-        load_default="ArgsSchema",
-    )
-
-
 class EURecordCuration(JobType):
     """EU Record Curation Job."""
 
-    arguments_schema = ArgsSchema
     task = run_eu_record_curation
     description = _("Curate published records for EU community")
     title = _("EU Record Curation")
     id = "eu_record_curation"
 
     @classmethod
-    def default_args(cls, job_obj, since=None, **kwargs):
+    def build_task_arguments(cls, job_obj, since=None, **kwargs):
         """Generate default job arguments here."""
         if since is None and job_obj.last_runs["success"]:
             since = job_obj.last_runs["success"].started_at
