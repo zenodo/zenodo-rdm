9691ee6cc91f940c7826f90dfdb6f232a26c825b
yashlamba <yashlamba2000@gmail.com>
2025-04-03 10:11:32 +0200

    communities_ui: add openness metric backend

--- site/zenodo_rdm/communities_ui/views/communities.py
+++ site/zenodo_rdm/communities_ui/views/communities.py
@@ -4,9 +4,9 @@
 # Copyright (C) 2019-2022 Northwestern University.
 # Copyright (C)      2022 TU Wien.
 #
-# Invenio App RDM is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
-"""Request views module."""
+# ZenodoRDM is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""Community custom views."""
 
 from flask import g, redirect, request, url_for
 from invenio_communities.views.communities import (
@@ -21,94 +21,16 @@ from invenio_rdm_records.proxies import (
 from invenio_rdm_records.resources.serializers import UIJSONSerializer
 from invenio_records_resources.services.errors import PermissionDeniedError
 
-
-def publications_metric_blr(results):
-    """Publications metric for BLR."""
-    resource_types = results._results.aggregations.resource_types.buckets
-    valid_pubication_types = [
-        "publication-article",
-        "publication-book",
-        "publication-section",
-        "publication-datamanagementplan",
-    ]
-    publications = 0
-    for resource_type in resource_types:
-        if resource_type.key in valid_pubication_types:
-            publications += resource_type.doc_count
-    return publications
+from .metrics_config import THEME_METRICS, THEME_METRICS_QUERY
 
 
-def images_metric_blr(results):
-    """Images metric for BLR."""
-    resource_types = results._results.aggregations.resource_types.buckets
-    images = 0
-    for resource_type in resource_types:
-        if resource_type.key.startswith("image"):
-            images += resource_type.doc_count
-    return images
-
-
-def treatments_metric_blr(results):
-    """Treatments metric for BLR."""
-    resource_types = results._results.aggregations.resource_types.buckets
-    for resource_type in resource_types:
-        if resource_type.key == "publication-taxonomictreatment":
-            return resource_type.doc_count
-
-
-def tables_metric_blr(results):
-    """Tables metric for BLR."""
-    resource_types = results._results.aggregations.resource_types.buckets
-    for resource_type in resource_types:
-        if resource_type.key == "dataset":
-            return resource_type.doc_count
-
-
-METRICS = {
-    "total_grants": {
-        "name": "total_grants",
-        "type": "cardinality",
-        "kwargs": {"field": "metadata.funding.award.id"},
-    },
-    "total_data": {
-        "name": "total_data",
-        "type": "sum",
-        "kwargs": {"field": "files.totalbytes"},
-    },
-    "total_views": {
-        "name": "total_views",
-        "type": "sum",
-        "kwargs": {"field": "stats.all_versions.unique_views"},
-    },
-    "total_downloads": {
-        "name": "total_downloads",
-        "type": "sum",
-        "kwargs": {"field": "stats.all_versions.unique_downloads"},
-    },
-    "resource_types": {
-        "name": "resource_types",
-        "type": "terms",
-        "kwargs": {"field": "metadata.resource_type.id"},
-    },
-}
-
-
-THEME_METRICS = {
-    "horizon": {"total_data": "total_data", "total_grants": "total_grants"},
-    "biosyslit": {
-        "total_views": "total_views",
-        "total_downloads": "total_downloads",
-        "publications": publications_metric_blr,
-        "images": images_metric_blr,
-        "tables": tables_metric_blr,
-        "treatments": treatments_metric_blr,
-    },
-}
-
-
-def _get_metric_from_search(result, metric):
+def _get_metric_from_search(result, accessor):
     """Get metric from search result."""
-    return result._results.aggregations[metric].value
+    value = result._results.aggregations
+    keys = accessor.split(".")
+    for key in keys:
+        value = value[key]
+    return value
 
 
 @pass_community(serialize=True)
@@ -136,7 +58,11 @@ def communities_home(pid_value, community, community_ui):
         recent_uploads = current_community_records_service.search(
             community_id=pid_value,
             identity=g.identity,
-            params={"sort": "newest", "size": 3, "metrics": METRICS},
+            params={
+                "sort": "newest",
+                "size": 3,
+                "metrics": THEME_METRICS_QUERY[community._record.theme["brand"]],
+            },
             expand=True,
         )
 
