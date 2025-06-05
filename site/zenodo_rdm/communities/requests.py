378ee264646e993e1cd9cd0156c863d90bb6a584
Fatimah Zulfiqar <fatimah0zulfiqar@gmail.com>
2025-06-05 23:14:52 +0200

    communities: added checks in submission and inclusion request

--- site/zenodo_rdm/communities/requests.py
+++ site/zenodo_rdm/communities/requests.py
@@ -8,8 +8,7 @@
 
 from datetime import datetime
 
-from invenio_checks.models import CheckConfig, CheckRun, CheckRunStatus
-from invenio_checks.proxies import current_checks_registry
+from invenio_checks.api import CheckRunAPI
 from invenio_rdm_records.proxies import current_rdm_records_service as service
 from invenio_rdm_records.requests import (
     community_inclusion,
@@ -29,8 +28,33 @@ class SubmissionSubmitAction(actions.SubmitAction):
         # Set the record's title as the request title.
         self.request["title"] = draft.metadata["title"]
 
-        # TODO: Run checks on record or draft
+        # Run checks on record or draft
+        if record.has_draft:
+            draft = service.draft_cls.pid.resolve(
+                record.pid.pid_value, registered_only=False
+            )
+            # Use the draft for checks
+            record = draft
 
+        # Run checks
+        CheckRunAPI.run_checks(
+            identity=identity,
+            record=record,
+        )
+
+        super().execute(identity, uow)
+
+class SubmissionCreateAction(actions.CreateAction):
+    """Create action."""
+
+    def execute(self, identity, uow):
+        """Execute the create action."""
+        draft = self.request.topic.resolve()
+        # Run checks
+        CheckRunAPI.run_checks(
+            identity=identity,
+            record=record,
+        )
         super().execute(identity, uow)
 
 
@@ -38,7 +62,7 @@ class CommunitySubmission(community_submission.CommunitySubmission):
     """Request to add a subcommunity to a Zenodo community."""
 
     available_actions = {
-        "create": actions.CreateAction,
+        "create": SubmissionCreateAction,
         "submit": SubmissionSubmitAction,
         "delete": actions.DeleteAction,
         "accept": community_submission.AcceptAction,
@@ -65,58 +89,11 @@ class InclusionSubmitAction(actions.SubmitAction):
             # Use the draft for checks
             record = draft
 
-        # FIXME: This has been copied from the checks component
-        community_ids = set()
-        community = self.request.receiver.resolve()
-        community_ids.add(str(community.id))
-        if community.parent:
-            community_ids.add(str(community.parent.id))
-
-        checks = CheckConfig.query.filter(
-            CheckConfig.community_id.in_(community_ids)
-        ).all()
-
-        for check in checks:
-            try:
-                check_cls = current_checks_registry.get(check.check_id)
-                start_time = datetime.utcnow()
-                res = check_cls().run(record, check)
-                if not res.sync:
-                    continue
-
-                latest_check = (
-                    CheckRun.query.filter(
-                        CheckRun.config_id == check.id,
-                        CheckRun.record_id == record.id,
-                    )
-                    # We prefer the latest non-draft check
-                    .order_by(CheckRun.is_draft.desc())
-                    .first()
-                )
-
-                if not latest_check:
-                    latest_check = CheckRun(
-                        config_id=check.id,
-                        record_id=record.id,
-                        is_draft=record.is_draft,
-                        revision_id=record.revision_id,
-                        start_time=start_time,
-                        end_time=datetime.utcnow(),
-                        status=CheckRunStatus.COMPLETED,
-                        state="",
-                        result=res.to_dict(),
-                    )
-                else:
-                    latest_check.is_draft = record.is_draft
-                    latest_check.revision_id = record.revision_id
-                    latest_check.start_time = start_time
-                    latest_check.end_time = datetime.utcnow()
-                    latest_check.result = res.to_dict()
-
-                # Create/update the check run to the database
-                uow.register(ModelCommitOp(latest_check))
-            except Exception:
-                continue
+        # Run checks
+        CheckRunAPI.run_checks(
+            identity=identity,
+            record=record,
+        )
 
         super().execute(identity, uow)
 
