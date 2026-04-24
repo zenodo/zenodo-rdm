871496a68c132b3cf6be6f003c4c1c756b8fb77c
Alex Ioannidis <a.ioannidis@cern.ch>
2026-04-25 00:13:24 +0200

    feat(assets): switch to pnpm and rspack

--- run-js-linter.sh
+++ run-js-linter.sh
@@ -5,18 +5,16 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-for arg in $@; do
+FIX=""
+
+for arg in "$@"; do
 	case ${arg} in
-		-i|--install)
-			npm install --no-save --no-package-lock @inveniosoftware/eslint-config-invenio@^2.0.0;;
-		-f|--fix)
-			printf "${GREEN}Run eslint${NC}\n";
-			npx eslint -c .eslintrc.yml site/zenodo_rdm/**/*.js --fix;;
+		-f|--fix) FIX="--fix";;
 		*)
 			printf "Argument ${RED}$arg${NC} not supported\n"
-			exit;;
+			exit 1;;
 	esac
 done
 
 printf "${GREEN}Run eslint${NC}\n"
-npx eslint -c .eslintrc.yml --ext .js site/
+npx --yes -p @inveniosoftware/eslint-config-invenio@2.0.0 -p eslint@^8 eslint -c .eslintrc.yml --ext .js ${FIX} site/
