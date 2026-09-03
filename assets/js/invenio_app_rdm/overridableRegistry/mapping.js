cbeb78a787278eac6d6d4ab696649c0da46102c5
Maira Salazar <mpssalazar@gmail.com>
2026-09-03 09:02:19 +0200

    refactor(js): remove override
    Instead of overriding the FeedbackLabel Popup com ponent, we make the
    change directly on react-invenio-forms.

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -4,13 +4,11 @@ import { UpgradeLegacyRecordButton } from "../../components/landing_page/overrid
 import SubcommunityCreateForm from "../../components/community/overrides/SubcommunityCreateForm";
 import FileModificationModalMessage from "../../components/community/overrides/FileModificationModalMessage";
 import { CustomAffiliationsSuggestions } from "../../components/react_invenio_forms/CustomAffiliationsSuggestions";
-import { FeedbackLabelPopup } from "../../components/react_invenio_forms/FeedbackLabelPopup";
 
 export const overriddenComponents = {
   "InvenioAppRdm.RecordLandingPage.RecordManagement.container":
     UpgradeLegacyRecordButton,
   "ReactInvenioForms.AffiliationsSuggestions.content": CustomAffiliationsSuggestions,
-  "ReactInvenioForms.FeedbackLabel.popup": FeedbackLabelPopup,
   "InvenioCommunities.CommunityCreateForm.layout": SubcommunityCreateForm,
   "InvenioAppRdm.Deposit.ModificationModal.message": FileModificationModalMessage,
 };
